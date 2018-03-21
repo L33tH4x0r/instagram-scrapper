4687ac312a6478f7c297b14df839eaab14b9d00c
austin <austin@dringo.io>
2018-03-21 00:01:16 -0700

    got loopthing through national parks and clearing form

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from urllib2 import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 from selenium import webdriver
+import time
+import csv
 
-get_park_names()
-
-browser = webdriver.Chrome("/home/austin/Projects/social_networks/instagram-scrapper/chromedriver")
-browser.get('https://instagram.com/')
-
-instagram_login(browser)
-
-instagram_base_url = "https://www.instagram.com/"
+instagram_base_url = "https://www.instagram.com"
 
 def get_park_names():
     wikipedia_url = "https://en.wikipedia.org/wiki/List_of_national_parks_of_the_United_States"
@@ -23,7 +18,7 @@ def get_park_names():
     num_parks = len(park_rows)
     parks = []
     for row in park_rows:
-        parks.append(row.contents[1].contents[0].contents[0].encode('ascii', 'ignore'))
+        parks.append(row.contents[1].contents[0].contents[0].encode('ascii', 'ignore') + " National Park")
     fo = open("national_parks", "wb")
     for park in parks:
         fo.write(park)
@@ -31,7 +26,59 @@ def get_park_names():
 
     fo.close()
 
+    return parks
+
 def instagram_login(browser):
     NEXT_BUTTON_XPATH = '//a[@href="/accounts/login/"]'
     button = browser.find_element_by_xpath(NEXT_BUTTON_XPATH)
     button.click()
+
+    user_name_form = browser.find_element_by_name("username")
+    user_name_form.send_keys("aturner9967@gmail.com")
+
+    password_form = browser.find_element_by_name("password")
+    password_form.send_keys("C35ar3_7411")
+
+    login_button = browser.find_element_by_css_selector("button._qv64e._gexxb._4tgw8._njrw0")
+    login_button.click()
+
+def get_url(park, browser):
+    root = browser.find_element_by_id("react-root")
+    root_html = root.get_attribute('innerHTML')
+    react_soup = soup(root_html, "html.parser")
+
+    links = []
+    for div in react_soup.find_all('a', href=True):
+        if "explore" in div["href"] and "locations"in div["href"]:
+            links.append(div)
+
+    for link in links[0:-1]:
+        if link.find_all("span")[0].contents[0].encode('ascii', 'ignore') == park:
+            return instagram_base_url + link["href"]
+
+def go_to_national_park(park, browser):
+    search_bar = browser.find_element_by_css_selector("input._avvq0._o716c")
+    search_bar.clear()
+    time.sleep(1)
+    search_bar.send_keys(park)
+    time.sleep(1)
+    return get_url(park, browser)
+
+def get_parks_urls(browser):
+    parks = get_park_names()
+    parks_list = [["Park", "Url"]]
+
+    for park in parks:
+        parks_list.append([park, go_to_national_park(park, browser)])
+
+    print parks_list
+
+def main():
+    browser = webdriver.Chrome("/home/austin/Projects/social_networks/instagram-scrapper/chromedriver")
+
+    browser.get(instagram_base_url)
+    instagram_login(browser)
+
+    get_parks_urls(browser)
+
+main()
